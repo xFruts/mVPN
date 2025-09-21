61e86fd7b8e32eb4483010d885e9bb212c33b4c1
xFruts <keksikiforlove@outlook.com>
2025-09-21 12:15:46 +0300

    Initial commit

---
+++ src/main/java/ru/maxow/mvpn/MVpnApplication.java
@@ -0,0 +1,13 @@
+package ru.maxow.mvpn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MVpnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MVpnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/maxow/mvpn/MVpnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maxow.mvpn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MVpnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
