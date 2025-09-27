c4dabdf3b8fd3f51b29e89d06427cbaf75ed46f7
frostg1g <vladryskin.bk.com@gmail.com>
2025-09-27 16:43:41 +0300

    feat(frame): The site skeleton has been created

--- mVPN-Frontend/src/App.tsx
+++ mVPN-Frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import Sidebar from "./pages/Sidebar/Sidebar.tsx";
+import MainContent from "./pages/MainContent/MainContent.tsx";
+import './App.css';
+
 function App() {
 
   return (
-    <>
-
-    </>
+    <div className="main-page">
+        <div className="sidebar">
+            <Sidebar />
+        </div>
+        <div className="main-content">
+            <MainContent />
+        </div>
+    </div>
   )
 }
 
