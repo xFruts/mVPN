fbd1d346d0582201063058f250f8332b08573859
xFruts <keksikiforlove@mail.ru>
2026-03-25 13:44:00 +0300

    refactor(security): enable CSRF protection

--- backend/src/main/java/ru/maxow/mvpn/configuration/SecurityConfig.java
+++ backend/src/main/java/ru/maxow/mvpn/configuration/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -44,7 +43,7 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .cors(Customizer.withDefaults())
-        .csrf(AbstractHttpConfigurer::disable)
+        .csrf(Customizer.withDefaults())
 
         .authorizeHttpRequests(auth -> auth
             // Swagger UI и API документация
